2938885407959e36e8394f48771494040bd2f305
= <=>
2022-12-15 12:07:47 +0100

    commiting day 3

---
+++ day03/day03.jl
@@ -0,0 +1,25 @@
+using Pipe: @pipe
+
+function day03(path::String)
+    input = readlines(path)
+    return part1(input), part2(input)
+
+end
+
+function find_duplicate(line)
+    return intersect(line[1:div(length(line),2)], line[div(length(line),2)+1 : end])[1]
+end
+
+function find_badge(lines)
+    return intersect(lines...)[1]
+end
+
+function part1(input)
+    return @pipe find_duplicate.(input) |> .+(_,isuppercase.(_)*58) |> .-(_,'`') |> sum
+end
+
+function part2(input)
+    return @pipe find_badge.([input[i:i+2] for i in 1:3:length(input)]) |> .+(_,isuppercase.(_)*58) |> .-(_,'`') |> sum
+end
+
+r = day03("day03/day03.txt")
